a92fa991572c52c52386fb6a7becfe6def0a52af
GodMinseok <mseok830@naver.com>
2025-02-10 21:37:30 +0900

    Section1. 프로젝트 환경설정(빌드하고 실행하기)

--- Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/InflearnSpringBeginnerApplication.java
+++ Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/InflearnSpringBeginnerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InflearnSpringBeginnerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(InflearnSpringBeginnerApplication.class, args);
 	}
