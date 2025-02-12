672e587175ead8d2634decf6ed01ec6d4122f08e
GodMinseok <mseok830@naver.com>
2025-02-12 17:32:42 +0900

    Section6. sql쿼리문 오류 수정

--- Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/SpringConfig.java
+++ Inflearn_Spring_Beginner/src/main/java/hello/Inflearn_Spring_Beginner/SpringConfig.java
@@ -4,6 +4,7 @@ import hello.Inflearn_Spring_Beginner.repository.JpaMemberRepository;
 import hello.Inflearn_Spring_Beginner.repository.MemberRepository;
 import hello.Inflearn_Spring_Beginner.service.MemberService;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,21 +12,23 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
 // return new JdbcMemberRepository(dataSource);
 // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
