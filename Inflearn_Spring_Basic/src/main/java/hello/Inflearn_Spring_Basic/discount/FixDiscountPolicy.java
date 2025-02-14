c378e1fc1bea48d9f12e074c45182338ab9291d5
GodMinseok <mseok830@naver.com>
2025-02-14 18:17:36 +0900

    Section2. 스프링 핵심 원리 이해1 - 예제 만들기(주문과 할인 도메인 개발)

---
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.Inflearn_Spring_Basic.discount;
+
+import hello.Inflearn_Spring_Basic.member.Grade;
+import hello.Inflearn_Spring_Basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; //1000원 할인
+    @Override
+    public int discount(Member member, int prince) {
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
