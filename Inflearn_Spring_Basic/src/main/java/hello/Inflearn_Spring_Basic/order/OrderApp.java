20c9c04f2d858ff976677cf24d06da20685dc1ad
GodMinseok <mseok830@naver.com>
2025-02-14 18:24:36 +0900

    Section2. 스프링 핵심 원리 이해1 - 예제 만들기(주문과 할인 도메인 실행과 테스트)

---
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/order/OrderApp.java
@@ -0,0 +1,21 @@
+package hello.Inflearn_Spring_Basic.order;
+
+import hello.Inflearn_Spring_Basic.member.Grade;
+import hello.Inflearn_Spring_Basic.member.Member;
+import hello.Inflearn_Spring_Basic.member.MemberService;
+import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
