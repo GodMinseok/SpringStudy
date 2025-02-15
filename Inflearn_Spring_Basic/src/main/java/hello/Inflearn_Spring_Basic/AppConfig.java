cd68fed580c5c36db8db3d8f8c978ecad7de360c
GodMinseok <mseok830@naver.com>
2025-02-15 11:01:38 +0900

    Section3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용(AppConfig 리팩터링)

---
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/AppConfig.java
@@ -0,0 +1,28 @@
+package hello.Inflearn_Spring_Basic;
+
+import hello.Inflearn_Spring_Basic.discount.DiscountPolicy;
+import hello.Inflearn_Spring_Basic.discount.FixDiscountPolicy;
+import hello.Inflearn_Spring_Basic.member.MemberService;
+import hello.Inflearn_Spring_Basic.member.MemberServiceImpl;
+import hello.Inflearn_Spring_Basic.member.MemoryMemberRepository;
+import hello.Inflearn_Spring_Basic.order.OrderService;
+import hello.Inflearn_Spring_Basic.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+}
