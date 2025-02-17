4f9bf41395c18350c85590643b0482429d13a334
GodMinseok <mseok830@naver.com>
2025-02-17 16:57:36 +0900

    Section7. 의존관계 자동 주입(생성자 주입을 선택해라!)

---
+++ Inflearn_Spring_Basic/src/test/java/hello/Inflearn_Spring_Basic/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package hello.Inflearn_Spring_Basic.order;
+
+import hello.Inflearn_Spring_Basic.discount.FixDiscountPolicy;
+import hello.Inflearn_Spring_Basic.member.Grade;
+import hello.Inflearn_Spring_Basic.member.Member;
+import hello.Inflearn_Spring_Basic.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        memoryMemberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
