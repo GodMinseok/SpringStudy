73e5ced19241f645a02ab5dd4f624776e4e57eb7
GodMinseok <mseok830@naver.com>
2025-02-17 23:30:51 +0900

    Section7. 의존관계 자동 주입(조회 빈이 2개 이상 - 문제)

--- Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/discount/FixDiscountPolicy.java
+++ Inflearn_Spring_Basic/src/main/java/hello/Inflearn_Spring_Basic/discount/FixDiscountPolicy.java
@@ -2,10 +2,13 @@ package hello.Inflearn_Spring_Basic.discount;
 
 import hello.Inflearn_Spring_Basic.member.Grade;
 import hello.Inflearn_Spring_Basic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
+
     @Override
     public int discount(Member member, int prince) {
         if(member.getGrade() == Grade.VIP) {
