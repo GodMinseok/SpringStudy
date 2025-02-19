0878eece7aed21e24b8e80dbe3cc7d480d895658
GodMinseok <mseok830@naver.com>
2025-02-19 21:43:24 +0900

    Section2. 서블릿(프로젝트 생성)

---
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/InflearnSpringMvc1Application.java
@@ -0,0 +1,13 @@
+package hello.Inflearn_Spring_MVC_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringMvc1Application.class, args);
+	}
+
+}
