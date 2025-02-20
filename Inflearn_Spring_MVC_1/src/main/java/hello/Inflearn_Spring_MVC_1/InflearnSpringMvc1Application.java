123e9400bd5831f999389c1f5607d89df41c78d7
GodMinseok <mseok830@naver.com>
2025-02-20 23:24:20 +0900

    Section2. 서블릿(프로젝트 오류 수정)

--- Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/InflearnSpringMvc1Application.java
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/InflearnSpringMvc1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InflearnSpringMvc1Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(InflearnSpringMvc1Application.class, args);
 	}
