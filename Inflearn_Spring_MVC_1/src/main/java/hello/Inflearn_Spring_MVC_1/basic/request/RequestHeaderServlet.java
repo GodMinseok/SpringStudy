90f6307c4363e0446fd044679172cf05d22a0cd1
GodMinseok <mseok830@naver.com>
2025-02-22 20:54:54 +0900

    Section2. 서블릿(HTTP 요청 데이터 - 개요)

--- Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/basic/request/RequestHeaderServlet.java
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/basic/request/RequestHeaderServlet.java
@@ -60,6 +60,7 @@ public class RequestHeaderServlet extends HttpServlet {
     //Header 편리한 조회
     private void printHeaderUtils(HttpServletRequest request) {
         System.out.println("--- Header 편의 조회 start ---");
+
         System.out.println("[Host 편의 조회]");
         System.out.println("request.getServerName() = " +
                 request.getServerName()); //Host 헤더
