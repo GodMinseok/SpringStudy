386772711aef90fe77d18e917e677ed38e4a799c
GodMinseok <mseok830@naver.com>
2025-02-22 20:31:20 +0900

    Section2. 서블릿(Hello 서블릿 추가)

--- Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/InflearnSpringMvc1Application.java
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/InflearnSpringMvc1Application.java
@@ -2,7 +2,9 @@ package hello.Inflearn_Spring_MVC_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class InflearnSpringMvc1Application {
 
