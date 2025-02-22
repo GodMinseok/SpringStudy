bc2b17309f2eedcb7ef1fb43d428118755f70aef
GodMinseok <mseok830@naver.com>
2025-02-23 01:57:43 +0900

    Section2. 서블릿(HTTP 요청 데이터 - API메시지 바디 - JSON)

---
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.Inflearn_Spring_MVC_1.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+}
