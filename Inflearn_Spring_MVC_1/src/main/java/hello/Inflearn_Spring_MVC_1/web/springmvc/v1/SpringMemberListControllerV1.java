0a8338ec8668c7cd6d8e1fa5c2e308b1f5f25cc4
GodMinseok <mseok830@naver.com>
2025-02-24 18:44:10 +0900

    Section5. 스프링 MVC - 구조 이해(스프링 MVC - 시작하기)

---
+++ Inflearn_Spring_MVC_1/src/main/java/hello/Inflearn_Spring_MVC_1/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package hello.Inflearn_Spring_MVC_1.web.springmvc.v1;
+
+import hello.Inflearn_Spring_MVC_1.domain.member.Member;
+import hello.Inflearn_Spring_MVC_1.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
