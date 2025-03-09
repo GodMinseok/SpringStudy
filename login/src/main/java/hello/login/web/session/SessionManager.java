e4687524ad0f7c86308cbd3a642c8527d7ed314a
GodMinseok <mseok830@naver.com>
2025-03-09 23:42:46 +0900

    Section7. 로그인 처리1 - 쿠키, 세션(쿠키와 보안 문제)

---
+++ login/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,38 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     * sessionId 생성 (임의 추정 불가능한 랜덤 값)
+     * 세션 저장소에 sessionId오 보관할 값 저장
+     * sessionId로 응답 쿠키를 생성해서 클라이언트 전달
+     */
+    public void createSession(Object value, HttpServletResponse response) {
+
+        //세션 id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        //쿠키 생성
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+
+    }
+
+}
