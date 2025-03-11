a36cf205f26e4d58f0ace218c958382c7df25f27
GodMinseok <mseok830@naver.com>
2025-03-11 18:57:35 +0900

    Section11. 스프링 타입 컨버터(포맷터를 지원하는 컨버전 서비스)

--- typeconverter/src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
+++ typeconverter/src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
@@ -3,7 +3,6 @@ package hello.typeconverter.converter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
-
 @Slf4j
 public class StringToIntegerConverter implements Converter<String, Integer> {
 
--- typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
+++ typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
@@ -12,7 +12,7 @@ public class StringToIpPortConverter implements Converter<String, IpPort> {
         log.info("convert source={}", source);
         //"127.0.0.1:8080" -> IpPort 객체
         String[] split = source.split(":");
-        String ip =  split[0];
+        String ip = split[0];
         int port = Integer.parseInt(split[1]);
         return new IpPort(ip, port);
     }
